3fb71db0c197c4ab8f9e1e453e59f6c7447c6187
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-12 15:00:54 +0300

    Loading all the stringified jobs info to the front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
-const HomePage = () => {
-	return <div>HomePage</div>;
+import { db } from "@/db/drizzle";
+import { jobs } from "@/db/schema";
+import { desc, eq } from "drizzle-orm";
+
+const HomePage = async () => {
+  const availableJobs = await db
+    .select()
+    .from(jobs)
+    .where(eq(jobs.approved, true))
+    .orderBy(desc(jobs.createdAt));
+
+  return (
+    <main className="h-full overflow-y-auto overflow-x-clip">
+      {JSON.stringify(availableJobs)}
+    </main>
+  );
 };
 
 export default HomePage;
