031e19765034f14df26659894d0597a66e36dfae
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-12 15:31:25 +0300

    Added the formatMobet and relativeDate utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { formatDistanceToNowStrict } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatMoney = (amount: number) =>
+  new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    maximumFractionDigits: 0,
+  }).format(amount);
+
+export const relativeDate = (from: Date) =>
+  formatDistanceToNowStrict(from, { addSuffix: true });
