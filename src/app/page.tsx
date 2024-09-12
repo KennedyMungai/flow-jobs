0d67809d6472b48d302e2ee9c9ef7127c2411ea6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-12 15:15:10 +0300

    Loading the job list item component inside an array map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
+import JobsListItem from "@/components/jobs-list-item";
 import { db } from "@/db/drizzle";
-import { jobs } from "@/db/schema";
+import { jobs, readJobsSchema } from "@/db/schema";
 import { desc, eq } from "drizzle-orm";
 
 const HomePage = async () => {
-  const availableJobs = await db
+  const availableJobsQuery = await db
     .select()
     .from(jobs)
     .where(eq(jobs.approved, true))
     .orderBy(desc(jobs.createdAt));
 
+  const availableJobs = readJobsSchema.array().parse(availableJobsQuery);
+
   return (
     <main className="h-full overflow-y-auto overflow-x-clip">
-      {JSON.stringify(availableJobs)}
+      {availableJobs.map((job) => (
+        <JobsListItem key={job.id} job={job} />
+      ))}
     </main>
   );
 };
