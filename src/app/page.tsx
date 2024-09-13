316249e287e4c20ed6d883b427f87971fb0ce474
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-13 10:29:44 +0300

    Added the sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import JobsFilterSidebar from "@/components/jobs-filter-sidebar";
 import JobsListItem from "@/components/jobs-list-item";
+import { ModeToggle } from "@/components/mode-toggle";
 import { db } from "@/db/drizzle";
 import { jobs, readJobsSchema } from "@/db/schema";
 import { desc, eq } from "drizzle-orm";
@@ -15,13 +17,17 @@ const HomePage = async () => {
   return (
     <main className="mx-auto my-10 h-full max-w-4xl space-y-10 overflow-y-auto overflow-x-clip px-3">
       <div className="space-y-5 text-center">
+        <div className="">
+          <ModeToggle />
+        </div>
         <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
           Developer Jobs
         </h1>
         <p className="text-muted-foreground">Find your dream job</p>
       </div>
-      <section>
-        <div className="space-y-4">
+      <section className="flex flex-col gap-4 md:flex-row">
+        <JobsFilterSidebar />
+        <div className="grow space-y-4">
           {availableJobs.map((job) => (
             <JobsListItem key={job.id} job={job} />
           ))}
