969b3dc283546e1c68bd2a424018a5fa5ff4ba97
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-13 10:35:04 +0300

    Created the jobs filter sidebar

---
+++ src/components/jobs-filter-sidebar.tsx
@@ -0,0 +1,51 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { db } from "@/db/drizzle";
+import { jobs } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import Select from "./select";
+
+const filterJobs = async (formData: FormData) => {
+  "use server";
+};
+
+const JobsFilterSidebar = async () => {
+  const distinctLocations = (await db
+    .selectDistinctOn([jobs.location], { location: jobs.location })
+    .from(jobs)
+    .where(eq(jobs.approved, true))
+    .then((locations) =>
+      locations.map(({ location }) => location).filter(Boolean),
+    )) as string[];
+
+  return (
+    <aside className="sticky top-0 h-fit rounded-lg border bg-background p-4 md:w-[260px]">
+      <form action={filterJobs}>
+        <div className="space-y-4">
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="q">Search</Label>
+            <Input
+              id="q"
+              name="q"
+              placeholder="Title, company, etc."
+              type="search"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="location">Location</Label>
+            <Select id="location" name="location" defaultValue={""}>
+              <option value="">All locations</option>
+              {distinctLocations.map((location) => (
+                <option key={location} value={location}>
+                  {location}
+                </option>
+              ))}
+            </Select>
+          </div>
+        </div>
+      </form>
+    </aside>
+  );
+};
+
+export default JobsFilterSidebar;
