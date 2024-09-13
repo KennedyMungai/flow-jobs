5a6093de880fac1c216d7beb7c3f1a983ae36a73
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-13 15:02:29 +0300

    Implemented an index for full text search

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,7 @@
+import { sql } from "drizzle-orm";
 import {
   boolean,
+  index,
   integer,
   pgTable,
   text,
@@ -9,23 +11,32 @@ import {
 } from "drizzle-orm/pg-core";
 import { createSelectSchema, createInsertSchema } from "drizzle-zod";
 
-export const jobs = pgTable("jobs", {
-  id: uuid("id").defaultRandom().primaryKey(),
-  slug: text("slug").unique().notNull(),
-  title: varchar("title", { length: 255 }).notNull(),
-  type: varchar("type", { length: 255 }).notNull(),
-  locationType: varchar("locationType", { length: 255 }).notNull(),
-  location: varchar("location", { length: 255 }),
-  description: text("description").notNull(),
-  salary: integer("salary").notNull(),
-  companyName: varchar("companyName", { length: 255 }).notNull(),
-  applicationEmail: varchar("applicationEmail", { length: 255 }),
-  applicationUrl: varchar("applicationUrl", { length: 255 }),
-  companyLogoUrl: varchar("companyLogoUrl", { length: 255 }),
-  approved: boolean("approved").default(false).notNull(),
-  createdAt: timestamp("createdAt").defaultNow().notNull(),
-  updatedAt: timestamp("updatedAt").$onUpdate(() => new Date()),
-});
+export const jobs = pgTable(
+  "jobs",
+  {
+    id: uuid("id").defaultRandom().primaryKey(),
+    slug: text("slug").unique().notNull(),
+    title: varchar("title", { length: 255 }).notNull(),
+    type: varchar("type", { length: 255 }).notNull(),
+    locationType: varchar("locationType", { length: 255 }).notNull(),
+    location: varchar("location", { length: 255 }),
+    description: text("description").notNull(),
+    salary: integer("salary").notNull(),
+    companyName: varchar("companyName", { length: 255 }).notNull(),
+    applicationEmail: varchar("applicationEmail", { length: 255 }),
+    applicationUrl: varchar("applicationUrl", { length: 255 }),
+    companyLogoUrl: varchar("companyLogoUrl", { length: 255 }),
+    approved: boolean("approved").default(false).notNull(),
+    createdAt: timestamp("createdAt").defaultNow().notNull(),
+    updatedAt: timestamp("updatedAt").$onUpdate(() => new Date()),
+  },
+  (table) => ({
+    jobSearchIndex: index("jobSearchIndex").using(
+      "gin",
+      sql`to_tsvector('english', ${table.title})`,
+    ),
+  }),
+);
 
 export const readJobsSchema = createSelectSchema(jobs).omit({
   updatedAt: true,
