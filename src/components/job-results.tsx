f484dbb8ddb444a84cc9a8b960b74fbb2cd1ff99
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-13 15:18:44 +0300

    Using more typesafe queries for the db search

--- src/components/job-results.tsx
+++ src/components/job-results.tsx
@@ -2,14 +2,50 @@ import "server-only";
 
 import { db } from "@/db/drizzle";
 import { jobs, readJobsSchema } from "@/db/schema";
-import { desc, eq } from "drizzle-orm";
+import { jobsFilterValues } from "@/lib/validation";
+import { and, desc, eq, not, sql } from "drizzle-orm";
 import JobsListItem from "./jobs-list-item";
 
-const JobResults = async () => {
+type Props = {
+  filterValues: jobsFilterValues;
+};
+
+const JobResults = async ({
+  filterValues: { q, location, remote, type },
+}: Props) => {
+  const searchString = q
+    ?.split(" ")
+    .filter((word) => word.length > 0)
+    .join(" & ");
+
+  const conditions = [eq(jobs.approved, true)];
+
+  if (searchString) {
+    conditions.push(
+      sql`to_tsvector('english', ${jobs.title}) @@ to_tsquery('english', ${sql`${searchString}`})`,
+    );
+  }
+
+  if (location) {
+    conditions.push(eq(jobs.location, location));
+  }
+
+  if (type) {
+    conditions.push(eq(jobs.type, type));
+  }
+
+  if (remote !== undefined) {
+    if (remote === true) {
+      conditions.push(eq(jobs.locationType, "Remote"));
+    } else if (remote === false) {
+      conditions.push(not(eq(jobs.locationType, "Remote")));
+    }
+  }
+
   const availableJobsQuery = await db
     .select()
     .from(jobs)
-    .where(eq(jobs.approved, true))
+    .where(and(...conditions))
     .orderBy(desc(jobs.createdAt));
 
   const availableJobs = readJobsSchema.array().parse(availableJobsQuery);
